b5ed6e78ccd3a9b79048d1459923e3f111108214
J0rg1J <jorg1hellenesj@gmail.com>
2026-06-05 11:13:31 +0200

    La til Supabase-klient i JavaScript-filen og implementerte funksjonalitet for å hente og vise produkter fra databasen.

--- script.js
+++ script.js
@@ -6,9 +6,41 @@ const tomHandlekurvKnapp = document.querySelector("#tom-handlekurv");
 const bestillKnapp = document.querySelector("#bestill-knapp");
 const meldingElement = document.querySelector("#melding");
 
+// Lager en forbindelse til Supabase ved å bruke URL og nøkkel
 const supabaseUrl = "https://vkuplcldclmcfcbtdlgf.supabase.co";
 const supabaseKey = "sb_publishable_8e_elINpvMIHGBLTdzzd0g_mzRXXS0K";
 
 const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-console.log("Supabase er koblet til");
+// denne funksjonen henter produktene fra databasen og putter dem i konsollen slik at visProdukter-funksjonen kan bruke dem til å vise dem på nettsiden
+async function hentProdukter() {
+  const { data, error } = await supabaseClient.from("produkter").select("*");
+
+  if (error) {
+    console.error("Feil ved henting av produkter:", error);
+    meldingElement.textContent = "Kunne ikke hente produkter.";
+    return;
+  }
+
+  console.log("Produkter hentet fra Supabase:", data);
+
+  visProdukter(data);
+}
+
+// dette får "produkter" delen av html-en til å vise produktene som er i databasen
+function visProdukter(produkter) {
+  produktListe.innerHTML = "";
+
+  produkter.forEach(function (produkt) {
+    produktListe.innerHTML += `
+      <article class="produkt-kort">
+        <h3>${produkt.navn}</h3>
+        <p>${produkt.beskrivelse}</p>
+        <p>${produkt.pris} kr</p>
+        <button>Legg i handlekurv</button>
+      </article>
+    `;
+  });
+}
+
+hentProdukter();
