c10174ab05721d24a51170d3466dfa2e4a2dda27
J0rg1J <jorg1hellenesj@gmail.com>
2026-06-05 10:30:50 +0200

    Oppdaterte kommentarer i HTML-filen og la til Supabase-klient i JavaScript-filen. La også til dokumentasjon for prosjektet.

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 const produktListe = document.querySelector("#produkt-liste");
 const handlekurvListe = document.querySelector("#handlekurv-liste");
 const totalPrisElement = document.querySelector("#total-pris");
+const tomHandlekurvKnapp = document.querySelector("#tom-handlekurv");
+const bestillKnapp = document.querySelector("#bestill-knapp");
+const meldingElement = document.querySelector("#melding");
 
-// tester at Js-filen er koblet riktig til HTML-en
-console.log("JavaScript er koblet til siden");
-console.log(produktListe);
-console.log(handlekurvListe);
-console.log(totalPrisElement);
+const supabaseUrl = "https://vkuplcldclmcfcbtdlgf.supabase.co";
+const supabaseKey = "sb_publishable_8e_elINpvMIHGBLTdzzd0g_mzRXXS0K";
+
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase er koblet til");
