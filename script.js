bdfacb09d3edef048981cd8ffe7cf14110c475e5
J0rg1J <jorg1hellenesj@gmail.com>
2026-06-05 10:05:02 +0200

    La til det viktigste for at javascript skal fungere.

---
+++ script.js
@@ -0,0 +1,10 @@
+// Henter html elementene som Javascript skal bruke senere
+const produktListe = document.querySelector("#produkt-liste");
+const handlekurvListe = document.querySelector("#handlekurv-liste");
+const totalPrisElement = document.querySelector("#total-pris");
+
+// tester at Js-filen er koblet riktig til HTML-en
+console.log("JavaScript er koblet til siden");
+console.log(produktListe);
+console.log(handlekurvListe);
+console.log(totalPrisElement);
